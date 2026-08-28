9fbba075a4ee71fcd767934a1ebb570908da36df
DansStuff <danlucas@live.ca>
2026-08-28 16:31:10 -0400

    feat: path tweaks

--- src/path/routedata.ts
+++ src/path/routedata.ts
@@ -32,7 +32,7 @@ export const SHIP_ROUTE = {
     },
     {
       name: 'Planet_4',
-      model: 'assets/scene/Models/TestPlanet2.gltf',
+      model: 'assets/scene/Models/TestPlanet.gltf',
       x: 6181.18,
       y: 0,
       z: 7981.74,
@@ -40,7 +40,7 @@ export const SHIP_ROUTE = {
     },
     {
       name: 'Planet_5',
-      model: 'assets/scene/Models/TestPlanet2.gltf',
+      model: 'assets/scene/Models/TestPlanet.gltf',
       x: 12040.88,
       y: 0,
       z: 1527.85,
@@ -48,7 +48,7 @@ export const SHIP_ROUTE = {
     },
     {
       name: 'Planet_6',
-      model: 'assets/scene/Models/TestPlanet2.gltf',
+      model: 'assets/scene/Models/TestPlanet.gltf',
       x: 20817.71,
       y: 0,
       z: -433.37,
@@ -56,7 +56,7 @@ export const SHIP_ROUTE = {
     },
     {
       name: 'Planet_7',
-      model: 'assets/scene/Models/TestPlanet2.gltf',
+      model: 'assets/scene/Models/TestPlanet.gltf',
       x: 9608.57,
       y: 0,
       z: -12799.7,
@@ -64,7 +64,7 @@ export const SHIP_ROUTE = {
     },
     {
       name: 'Planet_8',
-      model: 'assets/scene/Models/TestPlanet2.gltf',
+      model: 'assets/scene/Models/TestPlanet.gltf',
       x: 19279.05,
       y: 0,
       z: -8398.96,
@@ -84,42 +84,51 @@ export const SHIP_ROUTE = {
       points: [
         { x: -44, z: 184.42 },
         { x: -1256.83, z: 1672.51 },
-        { x: -1386.28, z: 4441.27 }
+        { x: -1324.9, z: 4433.08 }
       ]
     },
     {
       stopId: 'encounter-3',
       points: [
-        { x: -1386.28, z: 4441.27 },
-        { x: 5982.25, z: 8272.53 }
+        { x: -1324.9, z: 4433.08 },
+        { x: 1755.22, z: 7194.28 },
+        { x: 6079.79, z: 8318.73 }
       ]
     },
     {
       stopId: 'encounter-4',
       points: [
-        { x: 5982.25, z: 8272.53 },
-        { x: 11573.06, z: 1376.84 }
+        { x: 6079.79, z: 8318.73 },
+        { x: 8155.22, z: 5634.28 },
+        { x: 9795.22, z: 3194.28 },
+        { x: 11639.79, z: 1263.9 }
       ]
     },
     {
       stopId: 'encounter-5',
       points: [
-        { x: 11573.06, z: 1376.84 },
-        { x: 20923.6, z: -194.28 }
+        { x: 11639.79, z: 1263.9 },
+        { x: 15415.22, z: 834.28 },
+        { x: 18683.3, z: 618.12 },
+        { x: 21046.81, z: -184.01 }
       ]
     },
     {
       stopId: 'encounter-6',
       points: [
-        { x: 20923.6, z: -194.28 },
-        { x: 19197.59, z: -8079.29 }
+        { x: 21046.81, z: -184.01 },
+        { x: 21021.11, z: -3322.59 },
+        { x: 20321.11, z: -6062.59 },
+        { x: 19141.12, z: -8069.03 }
       ]
     },
     {
       stopId: 'encounter-7',
       points: [
-        { x: 19197.59, z: -8079.29 },
-        { x: 9770.78, z: -12893.75 }
+        { x: 19141.12, z: -8069.03 },
+        { x: 15821.11, z: -10062.59 },
+        { x: 12341.11, z: -11682.59 },
+        { x: 9822.11, z: -12934.81 }
       ]
     }
   ]
